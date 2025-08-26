9d7aee085dc22efc38711fc534f856088deba9a3
NAVADEEPKOMMINENI <23eg106b31@anurag.edu.in>
2025-08-26 13:47:58 +0530

    Update index.js

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Navadeep' });
+  res.render('index', { title: 'ProMeal Kitchen.' });
 });
 
 module.exports = router;
